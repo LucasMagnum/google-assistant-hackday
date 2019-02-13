ef2eb0fcc412ee8db3ef0e4ff2308e4af992bf26
Lucas Magnum <lucasmagnumlopes@gmail.com>
2019-02-13 15:29:28 +0100

    Testing response

--- data.py
+++ data.py
@@ -2,52 +2,52 @@ ebitda = [
   {
     "id": "continental-resources-inc-1",
     "name": "Continental Resources Inc",
-    "ebitda": "1.9B"
+    "ebitda": "1.9B$"
   },
   {
     "id": "concho-resources",
     "name": "Concho Resources",
-    "ebitda": "1.9B"
+    "ebitda": "1.9B$"
   },
   {
     "id": "ventas",
     "name": "Ventas",
-    "ebitda": "1.6B"
+    "ebitda": "1.6B$"
   },
   {
     "id": "public-storage-inc",
     "name": "A Public Storage Of Fayetteville Inc",
-    "ebitda": "1.5B"
+    "ebitda": "1.5B$"
   },
   {
     "id": "amc-networks",
     "name": "AMC Networks",
-    "ebitda": "1.5B"
+    "ebitda": "1.5B$"
   },
   {
     "id": "aarons",
     "name": "Aarons",
-    "ebitda": "1.5B"
+    "ebitda": "1.5B$"
   },
   {
     "id": "uk-power-networks-holdings-limited",
     "name": "Uk Power Networks Holdings Limited",
-    "ebitda": "1.4B"
+    "ebitda": "1.4B$"
   },
   {
     "id": "hindustan-zinc-limited",
     "name": "Hindustan Zinc Limited",
-    "ebitda": "1.4B"
+    "ebitda": "1.4B$"
   },
   {
     "id": "transdigm",
     "name": "TransDigm",
-    "ebitda": "1.3B"
+    "ebitda": "1.3B$"
   },
   {
     "id": "prologis",
     "name": "Prologis",
-    "ebitda": "1.3B"
+    "ebitda": "1.3B$"
   }
 ]
 
@@ -55,118 +55,97 @@ operating_revenue = [
   {
     "id": "intl-fcstone",
     "name": "INTL FCStone",
-    "operating_revenue": "24B"
+    "operating_revenue": "24B$"
   },
   {
     "id": "greenergy-fuels-limited",
     "name": "GREENERGY FUELS HOLDINGS LIMITED",
-    "operating_revenue": "16B"
+    "operating_revenue": "16B$"
   },
   {
     "id": "core-mark-international",
     "name": "Core-Mark International",
-    "operating_revenue": "13B"
+    "operating_revenue": "13B$"
   },
   {
     "id": "veritiv",
     "name": "Veritiv",
-    "operating_revenue": "6B"
+    "operating_revenue": "6B$"
   },
   {
     "id": "selfinvest-aps",
     "name": "Selfinvest Aps",
-    "operating_revenue": "6B"
+    "operating_revenue": "6B$"
   },
   {
     "id": "ocp-repartition",
     "name": "OCP Répartition",
-    "operating_revenue": "5B"
+    "operating_revenue": "5B$"
   },
   {
     "id": "datatec",
     "name": "Datatec",
-    "operating_revenue": "5B"
+    "operating_revenue": "5B$"
   },
   {
     "id": "tecnicas-reunidas",
     "name": "Técnicas Reunidas",
-    "operating_revenue": "5B"
+    "operating_revenue": "5B$"
   },
   {
     "id": "travelcenters-of-america-llc",
     "name": "Travelcenters Of America Llc",
-    "operating_revenue": "5B"
+    "operating_revenue": "5B$"
   },
   {
     "id": "kier-group-plc",
     "name": "Kier Group Plc",
-    "operating_revenue": "4.7B"
+    "operating_revenue": "4.7B$"
   }
 ]
 
 average_revenue = [
   {
     "cat": "consumer_robotics",
-    "avg_revenue_2017": "1.2592837795E9",
-    "avg_revenue_2016": "4.070183588333334E8",
-    "change": "2.093923780513433"
+    "avg_revenue_2017": "1.26 B$"
   },
   {
     "cat": "telemedicine",
-    "avg_revenue_2017": "1.0457636537142856E9",
-    "avg_revenue_2016": "4.001464058636364E8",
-    "change": "1.6134525723334012"
+    "avg_revenue_2017": "1.05 B$"
   },
   {
     "cat": "consumer_health_tech",
-    "avg_revenue_2017": "8.674715881052631E8",
-    "avg_revenue_2016": "5.2508211738000005E8",
-    "change": "0.6520684277607516"
+    "avg_revenue_2017": "867 M$"
   },
   {
     "cat": "electric_vehicles",
-    "avg_revenue_2017": "7.269724858E8",
-    "avg_revenue_2016": "5.433941467241378E8",
-    "change": "0.3378364308533093"
+    "avg_revenue_2017": "727 M$"
   },
   {
     "cat": "virtual_reality",
-    "avg_revenue_2017": "7.1442254375E8",
-    "avg_revenue_2016": "1.0189251795E9",
-    "change": "-0.29884690444044526"
+    "avg_revenue_2017": "714 M$"
   },
   {
     "cat": "5g_enablers",
-    "avg_revenue_2017": "7.124588168666667E8",
-    "avg_revenue_2016": "4.493122024838709E8",
-    "change": "0.5856654079904318"
+    "avg_revenue_2017": "712 M$"
   },
   {
     "cat": "coupons_and_deals",
-    "avg_revenue_2017": "7.001460446000001E8",
-    "avg_revenue_2016": "5.382998166250001E8",
-    "change": "0.3006618671909156"
+    "avg_revenue_2017": "700 M$"
   },
   {
     "cat": "gov_tech",
-    "avg_revenue_2017": "6.874084026486489E8",
-    "avg_revenue_2016": "2.831277371896552E8",
-    "change": "1.427909075500375"
+    "avg_revenue_2017": "687 M$"
   },
   {
     "cat": "online_retail",
-    "avg_revenue_2017": "6.462296896052632E8",
-    "avg_revenue_2016": "4.28362634625E8",
-    "change": "0.5086042464254377"
+    "avg_revenue_2017": "646 M$"
   },
   {
     "cat": "biopharma_contract_services",
-    "avg_revenue_2017": "6.071495399130436E8",
-    "avg_revenue_2016": "2.786485493313953E8",
-    "change": "1.1789079518621994"
+    "avg_revenue_2017": "607 M$"
   }
 ]
-
 
 top_growing = [
   {
