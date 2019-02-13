908adb884da9b3f3a8b12d2b671149bb2580e369
Lucas Magnum <lucasmagnumlopes@gmail.com>
2019-02-13 14:22:20 +0100

    Testing response

--- app.py
+++ app.py
@@ -30,22 +30,83 @@ def top_companies(parameters):
     operator = parameters["operator"]
 
     if operator.lower() == "earnings":
-        return{
+        return {
           "payload": {
             "google": {
               "expectUserResponse": True,
               "richResponse": {
-                "items": [
-                  {
-                    "simpleResponse": {
-                      "textToSpeech": "Howdy, this is GeekNum. I can tell you fun facts about almost any number, my favorite is 42. What number do you have in mind?",
-                      "displayText": "Howdy! I can tell you fun facts about almost any number. What do you have in mind?"
-                    }
-                  }
-                ]
+                "items": []
               },
+              "userStorage": "{\"data\":{}}",
+              "systemIntent": {
+                "intent": "actions.intent.OPTION",
+                "data": {
+                  "@type": "type.googleapis.com/google.actions.v2.OptionValueSpec",
+                  "listSelect": {
+                    "title": "List Title",
+                    "items": [
+                      {
+                        "optionInfo": {
+                          "key": "SELECTION_KEY_ONE",
+                          "synonyms": [
+                            "synonym 1",
+                            "synonym 2",
+                            "synonym 3"
+                          ]
+                        },
+                        "description": "This is a description of a list item.",
+                        "image": {
+                          "url": "IMG_URL_AOG.com",
+                          "accessibilityText": "Image alternate text"
+                        },
+                        "title": "Title of First List Item"
+                      },
+                      {
+                        "optionInfo": {
+                          "key": "SELECTION_KEY_GOOGLE_HOME",
+                          "synonyms": [
+                            "Google Home Assistant",
+                            "Assistant on the Google Home"
+                          ]
+                        },
+                        "description": "Google Home is a voice-activated speaker powered by the Google Assistant.",
+                        "image": {
+                          "url": "IMG_URL_GOOGLE_HOME.com",
+                          "accessibilityText": "Google Home"
+                        },
+                        "title": "Google Home"
+                      },
+                      {
+                        "optionInfo": {
+                          "key": "SELECTION_KEY_GOOGLE_PIXEL",
+                          "synonyms": [
+                            "Google Pixel XL",
+                            "Pixel",
+                            "Pixel XL"
+                          ]
+                        },
+                        "description": "Pixel. Phone by Google.",
+                        "image": {
+                          "url": "IMG_URL_GOOGLE_PIXEL.com",
+                          "accessibilityText": "Google Pixel"
+                        },
+                        "title": "Google Pixel"
+                      }
+                    ]
+                  }
+                }
+              }
             }
           },
+          "outputContexts": [
+            {
+              "name": "/contexts/_actions_on_google",
+              "lifespanCount": 99,
+              "parameters": {
+                "data": "{}"
+              }
+            }
+          ]
         }
 
     if operator.lower() == "revenue":
