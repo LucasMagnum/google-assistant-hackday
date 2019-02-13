06060481fc1170ab3f95fbf89e2731c9bd0b63c0
Lucas Magnum <lucasmagnumlopes@gmail.com>
2019-02-13 13:19:32 +0100

    Testing response

---
+++ data.py
@@ -0,0 +1,105 @@
+ebitda = [
+  {
+    "id": "continental-resources-inc-1",
+    "name": "Continental Resources Inc",
+    "ebitda": "1973949672"
+  },
+  {
+    "id": "concho-resources",
+    "name": "Concho Resources",
+    "ebitda": "1956141777"
+  },
+  {
+    "id": "ventas",
+    "name": "Ventas",
+    "ebitda": "1659048367"
+  },
+  {
+    "id": "public-storage-inc",
+    "name": "A Public Storage Of Fayetteville Inc",
+    "ebitda": "1565637178"
+  },
+  {
+    "id": "amc-networks",
+    "name": "AMC Networks",
+    "ebitda": "1508711451"
+  },
+  {
+    "id": "aarons",
+    "name": "Aarons",
+    "ebitda": "1504064572"
+  },
+  {
+    "id": "uk-power-networks-holdings-limited",
+    "name": "Uk Power Networks Holdings Limited",
+    "ebitda": "1422642629"
+  },
+  {
+    "id": "hindustan-zinc-limited",
+    "name": "Hindustan Zinc Limited",
+    "ebitda": "1420390501"
+  },
+  {
+    "id": "transdigm",
+    "name": "TransDigm",
+    "ebitda": "1399247079"
+  },
+  {
+    "id": "prologis",
+    "name": "Prologis",
+    "ebitda": "1375974807"
+  }
+]
+
+operating_revenue = [
+  {
+    "id": "intl-fcstone",
+    "name": "INTL FCStone",
+    "operating_revenue": "24922583260"
+  },
+  {
+    "id": "greenergy-fuels-limited",
+    "name": "GREENERGY FUELS HOLDINGS LIMITED",
+    "operating_revenue": "16766027416"
+  },
+  {
+    "id": "core-mark-international",
+    "name": "Core-Mark International",
+    "operating_revenue": "13080635015"
+  },
+  {
+    "id": "veritiv",
+    "name": "Veritiv",
+    "operating_revenue": "6974654358"
+  },
+  {
+    "id": "selfinvest-aps",
+    "name": "Selfinvest Aps",
+    "operating_revenue": "6193276890"
+  },
+  {
+    "id": "ocp-repartition",
+    "name": "OCP Répartition",
+    "operating_revenue": "5859660942"
+  },
+  {
+    "id": "datatec",
+    "name": "Datatec",
+    "operating_revenue": "5635326342"
+  },
+  {
+    "id": "tecnicas-reunidas",
+    "name": "Técnicas Reunidas",
+    "operating_revenue": "5067944000"
+  },
+  {
+    "id": "travelcenters-of-america-llc",
+    "name": "Travelcenters Of America Llc",
+    "operating_revenue": "5045939423"
+  },
+  {
+    "id": "kier-group-plc",
+    "name": "Kier Group Plc",
+    "operating_revenue": "4729330777"
+  }
+]
