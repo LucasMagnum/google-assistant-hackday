033b627af20103dcfb2d0253a9630c0c40911924
Lucas Magnum <lucasmagnumlopes@gmail.com>
2019-02-13 14:27:15 +0100

    Testing response

--- app.py
+++ app.py
@@ -31,82 +31,19 @@ def top_companies(parameters):
 
     if operator.lower() == "earnings":
         return {
-          "payload": {
-            "google": {
-              "expectUserResponse": True,
-              "richResponse": {
-                "items": []
-              },
-              "userStorage": "{\"data\":{}}",
-              "systemIntent": {
-                "intent": "actions.intent.OPTION",
-                "data": {
-                  "@type": "type.googleapis.com/google.actions.v2.OptionValueSpec",
-                  "listSelect": {
-                    "title": "List Title",
+            "payload": {
+                "google": {
+                  "expectUserResponse": True,
+                  "richResponse": {
                     "items": [
-                      {
-                        "optionInfo": {
-                          "key": "SELECTION_KEY_ONE",
-                          "synonyms": [
-                            "synonym 1",
-                            "synonym 2",
-                            "synonym 3"
-                          ]
-                        },
-                        "description": "This is a description of a list item.",
-                        "image": {
-                          "url": "IMG_URL_AOG.com",
-                          "accessibilityText": "Image alternate text"
-                        },
-                        "title": "Title of First List Item"
-                      },
-                      {
-                        "optionInfo": {
-                          "key": "SELECTION_KEY_GOOGLE_HOME",
-                          "synonyms": [
-                            "Google Home Assistant",
-                            "Assistant on the Google Home"
-                          ]
-                        },
-                        "description": "Google Home is a voice-activated speaker powered by the Google Assistant.",
-                        "image": {
-                          "url": "IMG_URL_GOOGLE_HOME.com",
-                          "accessibilityText": "Google Home"
-                        },
-                        "title": "Google Home"
-                      },
-                      {
-                        "optionInfo": {
-                          "key": "SELECTION_KEY_GOOGLE_PIXEL",
-                          "synonyms": [
-                            "Google Pixel XL",
-                            "Pixel",
-                            "Pixel XL"
-                          ]
-                        },
-                        "description": "Pixel. Phone by Google.",
-                        "image": {
-                          "url": "IMG_URL_GOOGLE_PIXEL.com",
-                          "accessibilityText": "Google Pixel"
-                        },
-                        "title": "Google Pixel"
-                      }
+                        {"simpleResponse": {"displayText": "Test 1"}}
+                        {"simpleResponse": {"displayText": "Test 2"}}
+                        {"simpleResponse": {"displayText": "Test 3"}}
+                        {"simpleResponse": {"displayText": "Test 4"}}
                     ]
-                  }
+                  },
                 }
-              }
             }
-          },
-          "outputContexts": [
-            {
-              "name": "/contexts/_actions_on_google",
-              "lifespanCount": 99,
-              "parameters": {
-                "data": "{}"
-              }
-            }
-          ]
         }
 
     if operator.lower() == "revenue":
